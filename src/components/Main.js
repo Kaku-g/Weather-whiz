160e20ee929187f2ad53c2d2e2aa36fa2009ea8f
jatin <rjjatinrawat9@gmail.com>
2023-05-25 02:39:10 +0530

    added icons and core functionality

---
+++ src/components/Main.js
@@ -0,0 +1,55 @@
+import React from "react";
+import WeatherIcon from "react-icons-weather";
+import {
+  WiThermometer,
+  WiCelsius,
+  WiWindBeaufort11,
+  WiHumidity,
+} from "react-icons/wi";
+const Main = ({ props }) => {
+  return (
+    <div className=" ">
+      <div className="grid grid-cols-2 md:grid-cols-4  gap-1">
+        <div className="flex flex-col justify-center items-center ease-in duration-300 hover:scale-110 cursor-pointer border-2 rounded-md mb-10 ml-10 mr-10 p-5 border-white-500  ">
+          <h1 className="text-white text-lg underline">Temperature</h1>
+
+          <div className="flex justify-center items-center">
+            <WiThermometer style={{ color: "white", fontSize: "30px" }} />
+            <p className="text-white">{props.temp} °C</p>
+          </div>
+        </div>
+        <div className="flex flex-col justify-center items-center ease-in duration-300 hover:scale-110 cursor-pointer border-2 rounded-md mb-10 ml-10 mr-10 p-5 border-white-500  ">
+          <h1 className="text-white text-lg underline">Conditions</h1>
+          <div className="flex justify-center items-center">
+            <WeatherIcon
+              className="p-5 text-white text-2xl"
+              name="owm"
+              flip="horizontal"
+              rotate="90"
+              iconId={props.icon_id || 200}
+            />
+            <p className="text-white">{props.cond}</p>
+          </div>
+        </div>
+        <div className="flex flex-col justify-center items-center ease-in duration-300 hover:scale-110 cursor-pointer border-2 rounded-md mb-10 ml-10 mr-10 p-5 border-white-500  ">
+          <h1 className="text-white text-lg underline">Humidity </h1>
+          <div className="flex justify-center items-center">
+            <WiHumidity style={{ color: "white", fontSize: "30px" }} />
+
+            <p className="text-white">{props.humidity}</p>
+          </div>
+        </div>
+        <div className="flex flex-col justify-center items-center ease-in duration-300 hover:scale-110 cursor-pointer border-2 rounded-md mb-10 ml-10 mr-10 p-5 border-white-500  ">
+          <h1 className="text-white text-lg underline">Windspeed </h1>
+          <div className="flex justify-center items-center">
+            <WiWindBeaufort11 style={{ color: "white", fontSize: "30px" }} />
+
+            <p className="text-white">{props.windspeed}m/s</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Main;
--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 import React, { useState } from "react";
 import { cities } from "../data/city";
+import Main from "./Main";
 
 const Weather = () => {
   const key = "4c86582d5af1c7895597ee6fd327356e";
@@ -8,6 +9,15 @@ const Weather = () => {
   const [weather, setWeather] = useState("");
   const [selectedCity, setSelectedCity] = useState("");
   const [cityMatch, setCityMatch] = useState([]);
+  const [clicked, setClicked] = useState(false);
+  // let style ="cursor-pointer text-white p-3 border border-solid border-white-500 block";
+  const handleClick = (item) => {
+    setSelectedCity(item);
+    // setClicked(true);
+    // style = clicked
+    // ? "cursor-pointer text-white p-3 border border-solid border-white-500 "
+    //: "cursor-pointer text-white p-3 border border-solid border-white-500 block";
+  };
 
   const getWeather = async () => {
     axios
@@ -15,8 +25,16 @@ const Weather = () => {
         `https://api.openweathermap.org/data/2.5/weather?q=${city},${city}&appid=${key}&units=metric`
       )
       .then((response) => {
-        setWeather(response["data"]);
-        console.log(response["data"]);
+        let data = response["data"];
+        console.log(data);
+        setWeather({
+          temp: data.main.temp,
+          humidity: data.main.humidity,
+          windspeed: data.wind.speed,
+          icon_id: data.weather[0].id,
+          cond: data.weather[0].main,
+          visib: data.visibility,
+        });
       });
   };
 
@@ -31,56 +49,72 @@ const Weather = () => {
   const updateCity = (value) => {
     setCity(value);
     searchCity(value);
+    setSelectedCity("");
+  };
+
+  const autoComplete = (item) => {
+    setCity(item);
+
+    handleClick(item);
   };
   return (
-    <div className="flex flex-col justify-center items-center mt-10  bg-gray-900 py-16 sm:py-24 lg:py-32 rounded-md p-30 m-20  ">
-      <div className=" flex flex-col jusitfy-center items-center  relative isolate overflow-hidden">
-        <h2 className="text-3xl font-bold tracking-tight text-white sm:text-4xl">
-          Weather Whiz
-        </h2>
-        <p className="mt-4 text-lg leading-8 text-gray-300">
-          Real time weather forcast
-        </p>
-        <div className="mt-6 flex max-w-md gap-x-4">
-          <label htmlFor="email-address" className="sr-only">
-            City Name
-          </label>
-          <input
-            id="city"
-            name="city"
-            type="text"
-            autoComplete="text"
-            required
-            className="min-w-0 flex-auto rounded-md border-0 bg-white/5 px-3.5 py-2 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6"
-            placeholder="Enter city "
-            value={city}
-            onChange={(e) => updateCity(e.target.value)}
-          />
-          <button
-            type="submit"
-            className="flex-none rounded-md bg-indigo-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
-            onClick={getWeather}
-          >
-            Find
-          </button>
+    <div className="bg-gray-900  rounded-md m-10 min-h-screen ">
+      <div className="flex flex-col justify-center items-center mt-10  py-16 sm:py-24 lg:py-32 rounded-md p-30 mb-5  ">
+        <div className=" flex flex-col jusitfy-center items-center  relative isolate overflow-hidden">
+          <h2 className="text-3xl font-bold tracking-tight text-white sm:text-4xl">
+            Weather Whiz
+          </h2>
+          <p className="mt-4 text-lg leading-8 text-gray-300">
+            Real time weather forcast
+          </p>
+          <div className="mt-6 flex max-w-md gap-x-4">
+            <label htmlFor="email-address" className="sr-only">
+              City Name
+            </label>
+            <input
+              id="city"
+              name="city"
+              type="text"
+              autoComplete="text"
+              required
+              className="min-w-0 flex-auto rounded-md border-0 bg-white/5 px-3.5 py-2 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6"
+              placeholder="Enter city "
+              value={city}
+              onChange={(e) => updateCity(e.target.value)}
+            />
+            <button
+              type="submit"
+              className="flex-none rounded-md bg-indigo-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
+              onClick={getWeather}
+            >
+              Find
+            </button>
+          </div>
+        </div>
+
+        <div class=" w-100 mt-4 max-h-[200px] max-w-[200px]   overflow-y-scroll">
+          <ul class="divide-y divide-gray-200">
+            {!selectedCity &&
+              city &&
+              cityMatch &&
+              cityMatch.map((item, index) => (
+                <li
+                  key={index}
+                  onClick={() => autoComplete(item)}
+                  className="cursor-pointer text-white p-3 border border-solid border-white-500"
+                >
+                  {item}
+                </li>
+              ))}
+          </ul>
         </div>
       </div>
-
-      <div class=" w-100 mt-4 max-h-[200px] max-w-[200px]   overflow-y-scroll">
-        <ul class="divide-y divide-gray-200">
-          {city &&
-            cityMatch &&
-            cityMatch.map((item, index) => (
-              <li
-                key={index}
-                onClick={() => setCity(item)}
-                className="cursor-pointer text-white p-3 border border-solid border-white-500  "
-              >
-                {item}
-              </li>
-            ))}
-        </ul>
-      </div>
+      {weather && <Main props={weather} />}
+      {!weather && !city && (
+        <h1 className="text-white text-center pb-5">
+          Search weather for your city
+        </h1>
+      )}
     </div>
   );
 };

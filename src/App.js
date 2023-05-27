28564e1fa726ce6fe790efd02ce1000abb836cf0
jatin <rjjatinrawat9@gmail.com>
2023-05-27 22:38:37 +0530

    fixed button posn

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 import Footer from "./components/Footer";
 function App() {
   return (
-    <div className="bg-gray-900   rounded-md pt-5 mt-5 m-10 min-h-screen">
+    <div className="bg-gray-900 pt-10  sm:rounded-md sm:pt-5 sm:mt-5 sm:m-10 min-h-screen">
       <Weather />
       <Footer />
     </div>

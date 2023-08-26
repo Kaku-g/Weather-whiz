d0ff9b44c93533167082efbe426d36059a711785
jatin <rjjatinrawat9@gmail.com>
2023-08-26 16:05:10 +0530

    added react-analytics

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import Weather from "./components/Weather";
 import "./App.css";
 import Footer from "./components/Footer";
+import { Track, TrackComponent } from "@kaku-g/react-analytics";
 function App() {
+  const TrackedWeather = TrackComponent(Weather);
+  const TrackedFooter = TrackComponent(Footer);
   return (
-    <div className="bg-gray-900 pt-10  sm:rounded-md sm:pt-5 sm:mt-5 sm:m-10 min-h-screen">
-      <Weather />
-      <Footer />
-    </div>
+    <Track userId="1eef16f5-33cb-4e44-b4de-7ea72fe9074d" project="weather">
+      <div className="bg-gray-900 pt-10  sm:rounded-md sm:pt-5 sm:mt-5 sm:m-10 min-h-screen">
+        <TrackedWeather />
+        <TrackedFooter />
+      </div>
+    </Track>
   );
 }
 
